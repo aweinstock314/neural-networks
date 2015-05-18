7e1f021336cf174b9c181b7bfd523c42334a3326
Avi Weinstock <aweinstock314@gmail.com>
2015-05-18 16:07:20 -0400

    Updated NeuralNetwork.hs to use an activation function.

--- NeuralNetwork.hs
+++ NeuralNetwork.hs
@@ -5,14 +5,26 @@ import Control.Monad
 import Control.Monad.Trans.State.Lazy
 import Numeric.LinearAlgebra.HMatrix
 import System.Random
+import qualified Data.Vector.Storable as V
 
-data NeuralNetwork a = NeuralNetwork { getWeightMatrices :: [Matrix a] } deriving Show
+-- ActivationFunction wraps a function with its derivative
+data ActivationFunction a = ActivationFunction (a -> a) (a -> a)
+
+tanhActivation = ActivationFunction tanh ((**2) . sech) where sech = (1/) . cosh
+
+data NeuralNetwork a = NeuralNetwork {
+    getWeightMatrices :: [Matrix a],
+    getActivationFunction :: ActivationFunction a
+    }
+
+instance (Element a, Show a) => Show (NeuralNetwork a) where
+    show (NeuralNetwork {getWeightMatrices = mats}) = "NeuralNetwork " ++ show mats
 
 -- (forwardPropagate nn x) returns the values at all the intermediate layers
 forwardPropagate :: Numeric a => NeuralNetwork a -> Vector a -> [Vector a]
-forwardPropagate (NeuralNetwork mats) x = scanl (flip app) x mats
+forwardPropagate (NeuralNetwork mats (ActivationFunction theta _)) x = scanl ((V.map theta .) . flip app) x mats
 
 makeRandomMatrix gen (m, n) range = runState (replicateM (m*n) (state (randomR range)) >>= return . matrix m) gen
 
-randomlyWeightedNetwork seed dims = NeuralNetwork . fst . foldr step ([], mkStdGen seed) $ zip dims (tail dims) where
+randomlyWeightedNetwork seed dims af = NeuralNetwork (fst . foldr step ([], mkStdGen seed) $ zip dims (tail dims)) af where
     step (m, n) (mats, gen) = first (:mats) (makeRandomMatrix gen (m, n) (0, 1))
