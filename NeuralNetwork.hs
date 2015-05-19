1af6b9b9fb4a2cc4afe7fc3f65d2c06ca9b15018
Avi Weinstock <aweinstock314@gmail.com>
2015-05-19 09:52:08 -0400

    Shortened constructor names for NeuralNetwork/ActivationFunction, and made ActivationFunction a Show instance. Simplify/generalize network initialization functions.

--- NeuralNetwork.hs
+++ NeuralNetwork.hs
@@ -1,30 +1,30 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 module NeuralNetwork where
-import Control.Arrow (first)
 import Control.Monad
 import Control.Monad.Trans.State.Lazy
 import Numeric.LinearAlgebra.HMatrix
 import System.Random
 import qualified Data.Vector.Storable as V
 
--- ActivationFunction wraps a function with its derivative
-data ActivationFunction a = ActivationFunction (a -> a) (a -> a)
+(<#>) = flip fmap
 
-tanhActivation = ActivationFunction tanh ((**2) . sech) where sech = (1/) . cosh
 
-data NeuralNetwork a = NeuralNetwork {
+-- ActivationFunction wraps a function with its derivative, along with a name (for printing)
+data ActivationFunction a = AF (a -> a) (a -> a) String
+instance Show (ActivationFunction a) where show (AF _ _ n) = "ActivationFunction " ++ n
+
+tanhActivation = AF tanh ((**2) . sech) "tanh/sech^2" where sech = (1/) . cosh
+
+data NeuralNetwork a = NN {
     getWeightMatrices :: [Matrix a],
     getActivationFunction :: ActivationFunction a
-    }
-
-instance (Element a, Show a) => Show (NeuralNetwork a) where
-    show (NeuralNetwork {getWeightMatrices = mats}) = "NeuralNetwork " ++ show mats
+    } deriving Show
 
 -- (forwardPropagate nn x) returns the values at all the intermediate layers
 forwardPropagate :: Numeric a => NeuralNetwork a -> Vector a -> [Vector a]
-forwardPropagate (NeuralNetwork mats (ActivationFunction theta _)) x = scanl ((V.map theta .) . flip app . V.cons 1) x mats
+forwardPropagate (NN mats (AF theta _ _)) x = scanl ((V.map theta .) . flip app . V.cons 1) x mats
 
-makeRandomMatrix gen (m, n) range = runState (replicateM (m*n) (state (randomR range)) >>= return . matrix m) gen
+initializeMatrix mkEntry (m, n) = runState (replicateM (m*n) (state mkEntry) <#> matrix m)
 
-randomlyWeightedNetwork seed dims af = NeuralNetwork (fst . foldr step ([], mkStdGen seed) $ zip dims (tail dims)) af where
-    step (m, n) (mats, gen) = first (:mats) (makeRandomMatrix gen (m+1, n) (0, 1))
+randomlyWeightedNetwork seed dims af = NN (evalState (mapM mkMat (zip dims (tail dims))) (mkStdGen seed)) af where
+    mkMat (m, n) = state $ initializeMatrix (randomR (0, 1)) (m+1,n)
