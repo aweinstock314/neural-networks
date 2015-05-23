f530ba9d835e70fa01b75e6b881fa5e777895017
Avi Weinstock <aweinstock314@gmail.com>
2015-05-23 16:34:22 -0400

    Transpose weight matrices to be consistent with other notation. Change range on weight generation. Include missing application of activation function in gradient calculations.

--- NeuralNetwork.hs
+++ NeuralNetwork.hs
@@ -21,24 +21,24 @@ data NeuralNetwork a = NN {
     } deriving Show
 
 applyNN :: Numeric a => NeuralNetwork a -> Vector a -> Vector a
-applyNN (NN mats (AF theta _ _)) x = foldl' ((cmap theta .) . flip app . V.cons 1) x mats
+applyNN (NN mats (AF theta _ _)) x = foldl' ((cmap theta .) . flip (app . tr) . V.cons 1) x mats
 
 -- (forwardPropagate nn x) returns the values at all the intermediate layers, before and after the activation function is applied
 forwardPropagate :: Numeric a => NeuralNetwork a -> Vector a -> ([Vector a], [Vector a])
 forwardPropagate (NN mats (AF theta _ _)) x = first tail . unzip $ scanl aux (undefined, V.cons 1 x) mats where
-    aux (_,z) w = let v = app w z in (v, V.cons 1 (cmap theta v))
+    aux (_,z) w = let v = app (tr w) z in (v, V.cons 1 (cmap theta v))
 
 -- (backPropagate nn x y) returns all the errors (in the same shape as the weights) of applying nn to x, with target value y
 backPropagate :: (Num (Vector a), Numeric a) => NeuralNetwork a -> Vector a -> Vector a -> [Matrix a]
 backPropagate nn@(NN mats (AF theta theta' _)) x y = gradient where
-    deltaStep (ds, d) (v, w) = let d' = (cmap theta' v) * (V.tail $ app (tr w) d) in (d':ds, d')
+    deltaStep (ds, d) (v, w) = let d' = (cmap theta' v) * (V.tail $ app w d) in (d':ds, d')
     (v:vs, zs) = first reverse $ forwardPropagate nn x
     ws = reverse mats
-    dLast = (cmap theta' v) * (y - v)
-    ds = fst $ foldl deltaStep ([dLast], dLast) (zip vs ws)
-    gradient = zipWith outer ds zs
+    dLast = (cmap theta' v) * (y - cmap theta v)
+    ds = fst $ foldl' deltaStep ([dLast], dLast) (zip vs ws)
+    gradient = zipWith outer zs ds
 
-stochasticGradientDescent dataset alpha nn = foldl update nn dataset where
+stochasticGradientDescent dataset alpha nn = foldl' update nn dataset where
     update nn@(NN oldWeights _) (x, y) = let
         gradient = backPropagate nn x y
         step = map (*scalar alpha) gradient
@@ -48,4 +48,4 @@ stochasticGradientDescent dataset alpha nn = foldl update nn dataset where
 initializeMatrix mkEntry (m, n) = runState (fmap (matrix m) $ replicateM (m*n) (state mkEntry))
 
 randomlyWeightedNetwork seed dims af = NN (evalState (mapM mkMat (zip dims (tail dims))) (mkStdGen seed)) af where
-    mkMat (m, n) = state $ initializeMatrix (randomR (0, 1)) (m+1,n)
+    mkMat (m, n) = state $ initializeMatrix (randomR (-1, 1)) (n,m+1)
