67c5d12293fc6b148a31e7a7b695381e3469ef5e
Avi Weinstock <aweinstock314@gmail.com>
2015-05-25 11:09:09 -0400

    Got the logistic activation function working (it seems to require a higher learning rate than the tanh activation function).

--- TestNeuralNetwork.hs
+++ TestNeuralNetwork.hs
@@ -6,22 +6,22 @@ import Numeric.LinearAlgebra.HMatrix
 import Text.Printf.TH
 import VisualizeFunction
 
-xorDataSet = map (vector *** vector) [
-    ([-1,-1], [-1]),
-    ([-1, 1], [ 1]),
-    ([ 1,-1], [ 1]),
-    ([ 1, 1], [-1])
-    ]
-
 andDataSet = map (vector *** vector) [
-    ([-1,-1], [-1]),
-    ([-1, 1], [ 1]),
-    ([ 1,-1], [ 1]),
-    ([ 1, 1], [ 1])
+    ([0,0], [0]),
+    ([0,1], [1]),
+    ([1,0], [1]),
+    ([1,1], [1])
     ]
 
-nn = randomlyWeightedNetwork 0 [2, 9, 1] tanhAF
-trainOn dataset = iterate (stochasticGradientDescent dataset 0.02) nn
+xorDataSet = map (vector *** vector) [
+    ([0,0], [0]),
+    ([0,1], [1]),
+    ([1,0], [1]),
+    ([1,1], [0])
+    ]
+
+nn = randomlyWeightedNetwork 0 [2, 9, 1] logisticAF
+trainOn dataset = iterate (stochasticGradientDescent dataset 0.25) nn
 
 andNNs = trainOn andDataSet
 xorNNs = trainOn xorDataSet
@@ -30,7 +30,7 @@ outputs dataset nns = map (\nn -> map (applyNN nn) (map fst dataset)) nns
 andOutputs = outputs andDataSet andNNs
 xorOutputs = outputs xorDataSet xorNNs
 
-image = visualizeFunctionBMP (-1,-1) (1,1) (1024,1024) (-1,1) . ((!0).) . applyNN
+image = visualizeFunctionBMP (0,0) (1,1) (1024,1024) (0,1) . ((!0).) . applyNN
 
 main = forM_ [0..10] $ \i -> do
     saveImage ([s|neuralnetwork_and_%04d.bmp|] (2^i)) (image (andNNs !! (2^i)))
